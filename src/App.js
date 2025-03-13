387a15605bea8d3e6c26e66f50fd213f0bab3389
aviral gupta <aviralg2002@gmail.com>
2025-03-13 09:29:59 +0530

    Chore: Changed app.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins with aviral gupta
+          Learn Jenkins with aviral gupta Not on udemmy
         </a>
       </header>
       <p>
